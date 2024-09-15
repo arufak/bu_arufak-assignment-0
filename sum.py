8609d3060d613ac722e74e19757355825c376169
Arufa Khanom <arufamanar1@gmail.com>
2024-09-15 16:31:56 -0400

    added sum.py

---
+++ sum.py
@@ -0,0 +1,13 @@
+# function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# input numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# calculate the sum
+sum_of_numbers = add_numbers(num1, num2)
+
+# print the sum
+print("The sum of the two numbers is:", sum_of_numbers)
